ea71644f2287d29a5cde2b36a0f690bc2c380af2
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2021-10-09 06:56:51 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {CovalentDynamicFormsModule} from "@covalent/dynamic-forms";
+import {CovalentLayoutModule} from "@covalent/core/layout";
+import {CovalentStepsModule} from "@covalent/core/steps";
+import {CovalentHighlightModule} from "@covalent/highlight";
+import {CovalentMarkdownModule} from "@covalent/markdown";
+import {CovalentBaseEchartsModule} from "@covalent/echarts/base";
+import {CovalentHttpModule} from "@covalent/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    CovalentBaseEchartsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
